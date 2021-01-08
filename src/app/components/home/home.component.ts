60a27b62352730b38a072fe26ad34e189cf93173
japc <japc.grafico@gmail.com>
2021-01-08 18:13:18 +0100

    Demo of the life cycle of a component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck,
+   AfterContentInit, AfterContentChecked, AfterViewInit,
+   AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -25,11 +27,42 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges, DoCheck,
+AfterContentInit, AfterContentChecked, AfterViewInit,
+AfterViewChecked, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(){
+    console.log("OnChanges");
+
+  }
+  ngDoCheck(){
+    console.log("DoCheck");
+
+  }
+  ngAfterContentInit(){
+    console.log("AfterContentInit");
+
+  }
+  ngAfterContentChecked(){
+    console.log("AfterContentChecked");
+
+  }
+  ngAfterViewInit(){
+    console.log("AfterViewInit");
+
+  }
+  ngAfterViewChecked(){
+    console.log("AfterViewChecked");
+
+  }
+  ngOnDestroy(){
+    console.log("OnDestroy");
+
   }
 
 }
